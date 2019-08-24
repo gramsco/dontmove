326be94c19cca2099078e600172408e6024bcf5c
gramsco <antoningauth@gmail.com>
2019-08-25 01:38:08 +0200

    dont rotate anymore

--- app.js
+++ app.js
@@ -3,26 +3,32 @@ let container = document.querySelector(".container")
 let squareMoving = document.getElementById("square_moving")
 let status = document.getElementById("status")
 
-for (let i = 0 ; i < 25 ; i++){
+for (let i = 0; i < 25; i++) {
     let newdiv = document.createElement("div");
-    if (i == 12) newdiv.className = "rotate joker"
+    if (i == 12) {
+    newdiv.className = "green"
+    }
+    else {
+    newdiv.className = "red"
+    }
     container.append(newdiv);
 }
 
-container.addEventListener("mousemove", (e) => {
+container.addEventListener("mouseover", (e) => {
 
     if (e.target == container) return false;
-    e.target.classList.toggle("rotate")
+    e.target.classList.toggle("red")
+    e.target.classList.toggle("green")
 
 })
 
 setInterval(() => {
-    let num = document.getElementsByClassName("rotate").length;
+    let num = document.getElementsByClassName("red").length;
     squareMoving.innerHTML = num;
-    if (num <= 0){
+    if (num <= 0) {
         status.innerHTML = "You win!"
     }
-    else{
+    else {
         status.innerHTML = "";
     }
 }, 50);
