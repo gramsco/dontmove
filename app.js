11d1f91d235b255892a2bdff6c351c7f227619ce
gramsco <antoningauth@gmail.com>
2019-08-25 01:50:09 +0200

    working

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ let container = document.querySelector(".container")
 let squareMoving = document.getElementById("square_moving")
 let status = document.getElementById("status")
 
-for (let i = 0; i < 25; i++) {
+
+
+
+let random1 = Math.floor(Math.random() * 400)
+let random2 = Math.floor(Math.random() * 400)
+let random3 = Math.floor(Math.random() * 400)
+let random4 = Math.floor(Math.random() * 400)
+
+for (let i = 0; i < 400 ; i++) {
     let newdiv = document.createElement("div");
-    if (i == 12) {
+    if (i == random1 || i == random2 || i == random3 || i == random4) {
     newdiv.className = "green"
     }
     else {
